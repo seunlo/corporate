15139952bee7b5f0363cb023372803be75c7c08f
seun jacobs <seunlo@gmail.com>
2024-05-23 15:53:48 +0100

    thank God

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -4,7 +4,9 @@ import header from "./header.png";
 import footer2 from "./footer2.png";
 import bird from "./bird.jpg";
 import desert from "./desert.jpg";
-import banner1 from "./banner1.png";
-import banner2 from "./banner2.jpeg";
+import banner1 from "./banner1.jpg";
+import banner2 from "./banner2.jpg";
+import tree from './tree.jpg'
+import text from './text.jpg'
 
-export { logo, footer, header, footer2, bird, desert, banner1, banner2 };
+export { logo, footer, header, footer2, bird, desert, banner1, banner2, tree, text };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,12 +4,12 @@ import Second from "../components/Second";
 //import Karousel from "../components/Karousel";
 import Color from "../components/Color";
 import Third from "../components/Third";
-import { bird, desert, banner1, banner2 } from "../assets/images";
+import { bird, desert, banner1, banner2, tree, text } from "../assets/images";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-const slides = [banner2];
+const slides = [banner1, banner2];
 
 function Home() {
   const settings = {
@@ -28,11 +28,13 @@ function Home() {
       {/* <Karousel /> */}
       <div className="">
         <Carousel autoSlide={true}>
-          {[
-            ...slides.map((s) => (
-              <img src={s} className="object-fit min-h-[300px] md:h-[600px] w-full" />
-            )),
-          ]}
+          {slides.map((s, index) => (
+            <img
+              key={index}
+              src={s}
+              className=""
+            />
+          ))}
         </Carousel>
       </div>
       {/* <First /> */}
